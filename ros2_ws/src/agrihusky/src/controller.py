94d32114e860082c394d76e15c28971ffe1dd4ab
fernand0labra <fernandolabracaso@gmail.com>
2025-05-28 09:28:28 +0200

    Update controller for waypoint selection

--- ros2_ws/src/agrihusky/src/controller.py
+++ ros2_ws/src/agrihusky/src/controller.py
@@ -12,8 +12,9 @@ from geometry_msgs.msg import Twist, Point, Quaternion, PointStamped
 
 ###
 
-inSimulation = True
-robotName = 'husky'
+
+withProbe = False     # Activate probe logic
+inSimulation = False  # Activate fake sensors
 
 # https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
 # Length in km of 1° of latitude = always 111.32 km
@@ -54,10 +55,10 @@ def euler_from_quaternion(quat):
 
 class HuskyController(Node):
     
-    def __init__(self, robotName, rate=10, maxSpeed=0.5, maxAngVel=0.5, goalDistance=0.2, slowDownDistance=0.7, turnInPlaceAngle=0.5):
+    def __init__(self, rate=10, maxSpeed=0.5, maxAngVel=0.5, goalDistance=0.2, slowDownDistance=0.7, turnInPlaceAngle=0.5):
         super().__init__('husky_controller')
 
-        self.robotName = robotName
+        self.robotName = 'Husky'
         self.rate = rate
         self.path = []
 
@@ -87,9 +88,9 @@ class HuskyController(Node):
 
         ###
 
-        self.cmdVelPub = self.create_publisher(Twist, '/husky_planner/cmd_vel', self.rate)                              # Velocity Command 
+        self.cmdVelPub = self.create_publisher(Twist, '/husky_planner/cmd_vel', self.rate)                                   # Velocity Command 
 
-        if not inSimulation:  # Services for probe handling
+        if not inSimulation and withProbe:  # Services for probe handling
             self.probeData = open(f"probe-data-{datetime.now():%Y%m%d-%H%M}.txt")
             self.probeClient = self.create_client(ProbeRequest, 'probe_request')
             while not self.probeClient.wait_for_service(timeout_sec=1.0):
@@ -101,12 +102,12 @@ class HuskyController(Node):
 
         self.gpsTopic = '/husky/gps' if inSimulation else '/ublox_client'
         self.gpsMessage = PointStamped if inSimulation else UbxNavPvt
-        self.poisitionSub = self.create_subscription(self.gpsMessage, self.gpsTopic, self.gpsCallback, self.rate)       # GPS Callback
+        self.poisitionSub = self.create_subscription(self.gpsMessage, self.gpsTopic, self.gpsCallback, self.rate)            # GPS Callback
 
-        self.orientationSub = self.create_subscription(Odometry, '/husky/odom', self.odometryCallback, self.rate) if inSimulation else \
-                              self.create_subscription(Quaternion, '/imu/data', self.imuCallback, self.rate)            # Odomemtry/IMU Callback
+        self.orientationSub = self.create_subscription(Odometry,   '/husky/odom',     self.odometryCallback, self.rate) if inSimulation else \
+                              self.create_subscription(Quaternion, '/imu/data/repub', self.imuCallback,      self.rate)      # Odomemtry/IMU Callback
         
-        self.waypointSub = self.create_subscription(Point, '/husky_planner/waypoint', self.waypointCallback, self.rate) # Waypoint Callback
+        self.waypointSub = self.create_subscription(Point, '/husky_planner/waypoint', self.waypointCallback, self.rate)      # Waypoint Callback
 
         self.timer = self.create_timer(1.0 / self.rate, self.control)  # Controller loop
         
@@ -116,10 +117,12 @@ class HuskyController(Node):
     def gpsCallback(self, msg):  # https://github.com/KumarRobotics/ublox/blob/ros2/ublox_msgs/msg/NavPVT.msg
         self.ubxNavPvt = msg
 
-        if self.latitude is None and self.longitude is None:
+        if self.latitude is None and self.longitude is None:  # When first latitude/longitude obtained
+            # Update global reference
             self.latitude  = msg.point.y if inSimulation else msg.lat
             self.longitude = msg.point.x if inSimulation else msg.lon
 
+            # Compute local offset in meters
             self.y_offset = self.latitude  * lat_degree_to_meter
             self.x_offset = self.longitude * lon_degree_to_meter(self.latitude)
 
@@ -130,9 +133,11 @@ class HuskyController(Node):
         lat_meters = (latitude  - self.latitude)  * lat_degree_to_meter
         lon_meters = (longitude - self.longitude) * lon_degree_to_meter(latitude  - self.latitude)
 
+        # Update local reference from latitude/longitude delta
         self.y = self.y + lat_meters  # Positive latitude  is North, negative is South
         self.x = self.x + lon_meters  # Positive longitude is East,  negative is West
 
+        # Update global reference
         self.latitude = latitude
         self.longitude = longitude
 
@@ -164,9 +169,11 @@ class HuskyController(Node):
         self.get_logger().info(f"Next waypoint: ({msg.x:.2f}, {msg.y:.2f})")
         self.segmentStart = np.array([self.x, self.y])
         
-        # Transform waypoint to local frame (based on initial GPS position)
+        # Transform waypoint to meters
         lat_meters = msg.y * lat_degree_to_meter
-        lon_meters = msg.x * lon_degree_to_meter(msg.y)       
+        lon_meters = msg.x * lon_degree_to_meter(msg.y)     
+
+        # Transform waypoint to local frame as a position from starting point (offset)
         self.segmentEnd = np.array([lon_meters - self.x_offset, lat_meters - self.y_offset])
 
         self.newWaypointData = True
@@ -190,13 +197,16 @@ class HuskyController(Node):
 
 
     def waypointRequest(self):
+        # Request new waypoint one meter forward in latitude
         latitude = meter_to_lat_degree(self.y + 0.1 * np.sign(self.y))
         longitude = meter_to_lon_degree(self.x, self.latitude)
 
+        # Build request message
         self.waypointMsgRequest.waypoint.z = 0.0
         self.waypointMsgRequest.waypoint.y = latitude
         self.waypointMsgRequest.waypoint.x = longitude
 
+        # Notify waypoint publisher
         response = self.waypointClient.call(self.waypointMsgRequest)
         self.get_logger().info(f"Waypoint Service Status: ({response.status})")
     
@@ -207,32 +217,37 @@ class HuskyController(Node):
 
             self.pose = np.array([[m.cos(self.angle), -m.sin(self.angle), self.x],
                                   [m.sin(self.angle),  m.cos(self.angle), self.y],
-                                  [0, 0, 1]])
+                                  [0, 0, 1]])  # Transformation matrix
 
             self.get_logger().info(f"Position: ({self.x:.2f}, {self.y:.2f}) | Angle: {self.angle:.2f}")
             # self.get_logger().info(f"Speed: {self.linVel} | Yawrate: {self.angVel:.2f}")
 
-            [xRefTrans, yRefTrans, _] = np.linalg.solve(self.pose, np.append(self.segmentEnd, 1))
+            [xRefTrans, yRefTrans, _] = np.linalg.solve(self.pose, np.append(self.segmentEnd, 1))  # Coordinate frame transform
             self.get_logger().info(f"Transformed waypoint: ({xRefTrans:.2f}, {yRefTrans:.2f})")
             
-            distance = np.linalg.norm([xRefTrans, yRefTrans])
+            distance = np.linalg.norm([xRefTrans, yRefTrans])  # Distance from origin
             cmdVel = Twist()
 
-            if distance < self.goalDistance:
+            if distance < self.goalDistance:  # When distance is close enough to the goal
                 self.newWaypointData = False
                 self.get_logger().info(f"Waypoint reached! ({self.segmentEnd[0]:.2f}, {self.segmentEnd[0]:.2f})")
 
-                if not inSimulation:
+                if not inSimulation and withProbe:  # Probe logic when indicated
                     result = self.probeRequest()
                     if result != None:  self.probeData.write(result)  # Write data if available
                     else:               self.waypointRequest()        # Otherwise adjust current waypoint
 
-            else:
-                refAngle = m.atan2(yRefTrans, xRefTrans)
-                if abs(refAngle) > self.turnInPlaceAngle:
-                    cmdVel.angular.z = np.sign(refAngle) * self.maxAngVel
+            else:                             
+                refAngle = m.atan2(yRefTrans, xRefTrans)   # Compute angle of the robot
+                if abs(refAngle) > self.turnInPlaceAngle:  # When robot requires turning in place
+                    cmdVel.angular.z = np.sign(refAngle) * self.maxAngVel  # Rotate at maxAngVel in the direction of refAngle
                 else:       
-                    cmdVel.linear.x = min(self.maxSpeed, self.maxSpeed * distance / self.slowDownDistance)
+                    # distance / self.slowDownDistance -> Ratio [0.0, 1.0]
+                    # Smaller linear velocities when distance below slowDownDistance otherwise maxSpeed
+                    cmdVel.linear.x = min(self.maxSpeed, self.maxSpeed * (distance / self.slowDownDistance))
+
+                    # abs(refAngle) / self.turnInPlaceAngle -> Ratio [0.0, 1.0]
+                    # Smaller angular velocities when refAngle below turnInPlaceAngle otherwise maxAngleVel
                     cmdVel.angular.z = self.maxAngVel * (abs(refAngle) / self.turnInPlaceAngle) * np.sign(refAngle)
                                         
             self.cmdVelPub.publish(cmdVel)
@@ -246,7 +261,7 @@ class HuskyController(Node):
 def main(args=None):
     try:
         rclpy.init(args=args)
-        huskyController = HuskyController(robotName, rate=25, maxSpeed=0.7, maxAngVel=1.2, goalDistance=0.1, slowDownDistance=0.5, turnInPlaceAngle=0.6)    
+        huskyController = HuskyController(rate=25, maxSpeed=0.7, maxAngVel=1.2, goalDistance=0.1, slowDownDistance=0.5, turnInPlaceAngle=0.6)    
         rclpy.spin(huskyController)
     except KeyboardInterrupt:
         pass
--- ros2_ws/src/agrihusky/src/waypoint_pub.py
+++ ros2_ws/src/agrihusky/src/waypoint_pub.py
@@ -13,18 +13,18 @@ class WaypointPublisher(Node):
     def __init__(self):
         super().__init__('waypoint_pub')
 
-        self.withData = True
+        self.withData = False  # Read every 60th GPS coordinate as waypoint
         self.path = []
-        if self.withData:  # Read every 60th GPS coordinate as waypoint
-            with open(get_package_share_directory('agrihusky') + '/data/gps-2025-05-20-13-37-14.txt', 'r') as file:
-                for ldx, line in enumerate(file.readlines()):
-                    if ldx%2 == 0:  
-                        self.path.append(line.split('\t'))  # [Latitude, Longitude]
+        self.filename = '/data/gps-2025-05-20-13-37-14.txt' if self.withData else '/data/new-waypoints.txt'
+        with open(get_package_share_directory('agrihusky') + self.filename, 'r') as file:
+            for ldx, line in enumerate(file.readlines()):
+                # if ldx%2 == 0:  
+                self.path.append(line.split('\t'))  # [Latitude, Longitude]
 
         self.waypoint_pub = self.create_publisher(Point, '/husky_planner/waypoint', 10)
         self.srv = self.create_service(WaypointRequest, 'waypoint_request', self.waypointCallback)
 
-        # self.timer = self.create_timer(2, self.publish_waypoint)  # TODO Communication regarding waypoint arrival between controller and publisher
+        self.timer = self.create_timer(2, self.publish_waypoint)  # TODO Communication regarding waypoint arrival between controller and publisher
 
 
     def waypointCallback(self, request, response):
@@ -38,18 +38,12 @@ class WaypointPublisher(Node):
     def publish_waypoint(self):
         waypoint = Point()
 
-        if self.withData:  # In global frame
-            if len(self.path) > 0:  coordinates = self.path.pop(0)
-            else:                   return
-            
-            waypoint.x = float(coordinates[1])
-            waypoint.y = float(coordinates[0])
-            waypoint.z = 0.0
+        if len(self.path) > 0:  coordinates = self.path.pop(0)
+        else:                   return
 
-        else:  # In local frame
-            waypoint.x = random.uniform(-10.0, 10.0)
-            waypoint.y = random.uniform(-10.0, 10.0)
-            waypoint.z = 0.0
+        waypoint.y = float(coordinates[0])
+        waypoint.x = float(coordinates[1])
+        waypoint.z = 0.0
 
         self.get_logger().info(f'Publishing waypoint: ({waypoint.x}, {waypoint.y}, {waypoint.z})')
         self.waypoint_pub.publish(waypoint)
@@ -61,7 +55,7 @@ def main(args=None):
     waypoint_publisher = WaypointPublisher()
 
     try:
-        waypoint_publisher.publish_waypoint()
+        # waypoint_publisher.publish_waypoint()
         rclpy.spin(waypoint_publisher)
     except KeyboardInterrupt:
         pass
